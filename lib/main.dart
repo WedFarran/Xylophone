8dccd9de3488627d72caf134e8d2b6c81d2e6b05
ˍıWéd Farrāи <wedfarran6@hotmail.com>
2023-11-16 15:38:49 +0300

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +10,76 @@ class Xylophone extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    playSound(sound) async {
+      final player = AudioPlayer();
+      await player.play(
+          AssetSource('note$sound.wav')); // will immediately start playing
+    }
+
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: SafeArea(
+              child: Column(
+            children: [
+              Expanded(
+                child: InkWell(
+                  onTap: () => playSound(1),
+                  child: Container(
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: InkWell(
+                  onTap: () => playSound(2),
+                  child: Container(
+                    color: Colors.orange,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: InkWell(
+                  onTap: () => playSound(3),
+                  child: Container(
+                    color: Colors.yellow,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: InkWell(
+                  onTap: () => playSound(4),
+                  child: Container(
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: InkWell(
+                  onTap: () => playSound(5),
+                  child: Container(
+                    color: Colors.teal,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: InkWell(
+                  onTap: () => playSound(6),
+                  child: Container(
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: InkWell(
+                  onTap: () => playSound(7),
+                  child: Container(
+                    color: Colors.purple,
+                  ),
+                ),
+              ),
+            ],
+          )),
+        ));
   }
 }
